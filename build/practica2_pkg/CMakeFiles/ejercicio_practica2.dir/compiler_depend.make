0e5a2a798959128f0cbb3adef3f966b3a9f653ad
roboticafacil <roboticafacil.es@gmail.com>
2025-03-20 15:53:56 +0100

    practica4_pkg install log build removed

--- build/practica2_pkg/CMakeFiles/ejercicio_practica2.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for ejercicio_practica2.
-# This may be replaced when dependencies are built.
--- build/turtlebot3_coppeliasim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leo/RM_prac/src/turtlebot3_coppeliasim
-# Build directory: /home/leo/RM_prac/build/turtlebot3_coppeliasim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
