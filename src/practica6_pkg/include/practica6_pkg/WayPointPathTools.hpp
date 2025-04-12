51deb365e99aa11d8f688e2c840952ae113a5a38
Leo Armesto <larmesto@idf.upv.es>
2025-04-12 14:10:11 +0200

    practica6 updated

--- src/practica6_pkg/include/practica6_pkg/WayPointPathTools.hpp
+++ src/practica6_pkg/include/practica6_pkg/WayPointPathTools.hpp
@@ -13,6 +13,7 @@ class WayPointPathTools
     WayPointPathTools(practica6_pkg::msg::WayPointPath, double);
     
     geometry_msgs::msg::Point findClosestPointOnPath(double *dClosest, double x_rob, double y_rob);
+    private:
     geometry_msgs::msg::Point p2sLookAheadDistance(const geometry_msgs::msg::Point& p1, const geometry_msgs::msg::Point& p2, const geometry_msgs::msg::Point& pi, double *d);
     std::vector<double> calculateRoots(const double &a0,const double &a1,const double &a2,const double &a3);
 };
