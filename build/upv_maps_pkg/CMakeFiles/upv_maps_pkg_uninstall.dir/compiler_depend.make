ef78897e0783796a8524b1e9ffd3a75f341e53cb
rinese89 <rinese@etsii.upv.es>
2026-04-15 12:47:32 +0200

    delete compilation folders

--- build/upv_maps_pkg/CMakeFiles/upv_maps_pkg_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for upv_maps_pkg_uninstall.
-# This may be replaced when dependencies are built.
--- install/pure_pursuit_pkg/share/pure_pursuit_pkg/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(pure_pursuit_pkg_IDL_FILES "msg/WayPointPath.idl")
-set(pure_pursuit_pkg_INTERFACE_FILES "msg/WayPointPath.msg")
